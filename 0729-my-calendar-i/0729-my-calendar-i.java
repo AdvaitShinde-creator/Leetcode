611d6650723680dd533c4cde0bfa4718e2ee73a5
AdvaitShinde-creator <shindeadvait333@gmail.com>
2026-07-23 08:22:45 +0530

    Time: 86 ms (19.32%), Space: 47 MB (91.11%) - LeetHub

---
+++ 0729-my-calendar-i/0729-my-calendar-i.java
@@ -0,0 +1,21 @@
+class MyCalendar {
+
+    List<int[]> bookings;
+
+    public MyCalendar() {
+        bookings = new ArrayList<>();
+    }
+
+    public boolean book(int start, int end) {
+
+        for (int[] booking : bookings) {
+
+            if (start < booking[1] && end > booking[0]) {
+                return false;
+            }
+        }
+
+        bookings.add(new int[]{start, end});
+        return true;
+    }
+}
