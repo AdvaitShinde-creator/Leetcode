ce088564d2ed331a7989a1f39c913e16f13f6abe
AdvaitShinde-creator <shindeadvait333@gmail.com>
2026-07-23 10:06:28 +0530

    Time: 10 ms (10.14%), Space: 45.6 MB (69.18%) - LeetHub

---
+++ 0657-robot-return-to-origin/0657-robot-return-to-origin.java
@@ -0,0 +1,25 @@
+class Solution {
+    public boolean judgeCircle(String moves) {
+        int up = 0;
+        int down = 0;
+        int right = 0;
+        int left = 0;
+
+        for(int i = 0 ; i < moves.length(); i++){
+            if(moves.charAt(i) == 'U'){
+                up++;
+            }
+            else if(moves.charAt(i) == 'L'){
+                left++;
+            }
+            else if(moves.charAt(i) == 'R'){
+                right++;
+            }
+            else{
+                down++;
+            }
+        }
+
+        return(left == right && up == down);
+    }
+}
