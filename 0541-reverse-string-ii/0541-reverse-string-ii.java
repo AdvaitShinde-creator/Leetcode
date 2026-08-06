a61fe9c02c0d846257f9b22ce07c889b4791c7b6
AdvaitShinde-creator <shindeadvait333@gmail.com>
2026-08-06 22:28:08 +0530

    Time: 4 ms (6.24%), Space: 44.8 MB (45.14%) - LeetHub

--- 0541-reverse-string-ii/0541-reverse-string-ii.java
+++ 0541-reverse-string-ii/0541-reverse-string-ii.java
@@ -1,15 +1,22 @@
 class Solution {
     public String reverseStr(String s, int k) {
-        char[] a = s.toCharArray();
-        for (int i = 0; i < a.length; i += 2 * k) {
-            int left = i;
-            int right = Math.min(i + k - 1, a.length - 1);
-            while (left < right) {
-                char temp = a[left];
-                a[left++] = a[right];
-                a[right--] = temp;
+        StringBuilder sb = new StringBuilder();
+        int n = s.length();
+        int i = 0;
+        while (i < n) {
+            int end = Math.min(i + k, n);
+            // reverse s[i..end-1]
+            for (int j = end - 1; j >= i; j--) {
+                sb.append(s.charAt(j));
             }
+            // append remaining non-reversed part? Actually in 2k block, after k reversed,
+            // we append the rest unchanged up to 2k-1
+            int remain = Math.min(i + 2 * k, n);
+            for (int j = end; j < remain; j++) {
+                sb.append(s.charAt(j));
+            }
+            i += 2 * k;
         }
-        return new String(a);
+        return sb.toString();
     }
 }
